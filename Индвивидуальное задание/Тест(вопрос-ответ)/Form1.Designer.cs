9813f9aa10fc46f930c18d9cae162bd1e0414ad0
kR0N3R <94796781+kR0N3R@users.noreply.github.com>
2021-11-27 11:17:58 +0300

    Add files via upload

---
+++ Индвивидуальное задание/Тест(вопрос-ответ)/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace Тест_вопрос_ответ_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(72, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(10, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = " ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Вопрос:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Location = new System.Drawing.Point(28, 60);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(475, 193);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Варианты ответов";
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(6, 80);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(14, 13);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 32);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(14, 13);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 56);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(14, 13);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(123, 259);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Проверить ответ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(123, 288);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(200, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Следующий вопрос";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(28, 260);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 51);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Начать тест";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 322);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Тест ";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Индвивидуальное задание/Тест(вопрос-ответ)/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Тест_вопрос_ответ_
+{
+    public partial class Form1 : Form
+    {
+          
+        List<Questions> ListQuestions = new List<Questions>();
+
+        
+        List<Answers> ListAnswers = new List<Answers>();
+        
+        // Файл со списком вопросов
+        string InputFileNameQuestions = @"questions.txt";
+
+        // Файл со списком ответов
+        string InputFileNameAnswers = @"answers.txt";
+
+        // Счетчик вопросов
+        int CountQuestions = 1;
+
+        // Правильный ответ
+        int TrueAnswer = 1;
+
+        int MaxQuestionNumber = 0;
+        public Form1()
+        {
+            InitializeComponent();
+
+            
+            ListDataQuestions(InputFileNameQuestions);
+
+            // Получили данные при запуске программы
+            ListDataAnswers(InputFileNameAnswers);
+
+            MaxQuestionNumber = ListQuestions.Max(m => m.QuestionNumber);
+
+            groupBox1.Visible = false;
+
+            button1.Visible = false;
+
+            button2.Visible = false;
+
+            label2.Visible = false;
+        }
+
+        // Проверка правильности ответа
+        private void button1_Click(object sender, EventArgs e)
+        {
+            radioButton1.ForeColor = Color.Black;
+
+            radioButton2.ForeColor = Color.Black;
+
+            radioButton3.ForeColor = Color.Black;
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (((RadioButton)groupBox1.Controls[i]).Checked)
+                {
+                    if (ListAnswers.Where(x => x.AnswerCorrect == 1).Any(w => w.AnswerText.Equals(groupBox1.Controls[i].Text.ToString())))
+                    {
+                        groupBox1.Controls[i].ForeColor = Color.Green;
+
+                        MessageBox.Show("Правильный ответ");
+                    }
+                    else
+                    {
+                        groupBox1.Controls[i].ForeColor = Color.Red;
+
+                        var TrueAnswer = ListAnswers.Where(x => x.AnswerCorrect == 1 && x.QuestionNumber == CountQuestions).Select(s => s.AnswerText);
+
+                        foreach (string Row in TrueAnswer)
+                        {
+                            for (int j = 0; j < 3; j++)
+                            {
+                                if (groupBox1.Controls[j].Text.ToString() == Row)
+                                {
+                                    groupBox1.Controls[j].ForeColor = Color.Green;
+                                }
+                            }
+                        }
+                        MessageBox.Show($"Не правильный ответ");
+                    }
+                }
+            }
+            CountQuestions++;
+
+            button1.Enabled = false;
+
+            button2.Enabled = true;
+        }
+
+        // Следующий вопрос
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (MaxQuestionNumber < CountQuestions)
+            {
+                MessageBox.Show("Вы завершили тест");
+            }
+            else
+            {
+                GetDataQuestionsAndAnswers();
+
+                radioButton1.ForeColor = Color.Black;
+
+                radioButton2.ForeColor = Color.Black;
+
+                radioButton3.ForeColor = Color.Black;
+
+                button1.Enabled = true;
+
+                button2.Enabled = false;
+            }
+        }
+
+        // Начать тест
+        private void button3_Click(object sender, EventArgs e)
+        {
+            groupBox1.Visible = true;
+
+            button1.Visible = true;
+
+            button2.Visible = true;
+
+            label2.Visible = true;
+
+            button3.Enabled = false;
+
+            button2.Enabled = false;
+
+            GetDataQuestionsAndAnswers();
+
+            
+        }
+
+        // Получаем список ответов
+        List<Answers> ListDataAnswers(string InputFileNameAnswers)
+        {
+            string[] FromFile = File.ReadAllLines(InputFileNameAnswers, Encoding.Default);
+
+            Answers AnswersInfo = new Answers();
+
+            for (int x = 0; x < FromFile.Length; x++)
+            {
+                var Data = FromFile[x].Split(';');
+
+                AnswersInfo.QuestionNumber = Convert.ToInt32(Data[0]);
+
+                AnswersInfo.AnswerText = Data[1];
+
+                AnswersInfo.AnswerCorrect = Convert.ToInt32(Data[2]);
+
+                ListAnswers.Add(AnswersInfo);
+            }
+            return ListAnswers;
+        }
+
+        // Получаем список вопросов
+        List<Questions> ListDataQuestions(string InputFileNameQuestions)
+        {
+            string[] FromFile = File.ReadAllLines(InputFileNameQuestions, Encoding.Default);
+
+            Questions QuestionsInfo = new Questions();
+
+            for (int x = 0; x < FromFile.Length; x++)
+            {
+                var Data = FromFile[x].Split(';');
+
+                QuestionsInfo.QuestionNumber = Convert.ToInt32(Data[0]);
+
+                QuestionsInfo.QuestionText = Data[1];
+
+                ListQuestions.Add(QuestionsInfo);
+            }
+            return ListQuestions;
+        }
+
+        // Заполняем контролы формы данными
+        void GetDataQuestionsAndAnswers ()
+        {
+            var SelectQuestion = ListQuestions.Where(w => w.QuestionNumber == CountQuestions).Select(s => new { s.QuestionText, s.QuestionNumber });
+
+            foreach (var Question in SelectQuestion)
+            {
+                label1.Text = Question.QuestionNumber + ".  " + Question.QuestionText;
+            }
+
+            var SelectAnswers = ListAnswers.Where(w => w.QuestionNumber == CountQuestions).Select(s =>  s.AnswerText);
+
+            radioButton1.Text = SelectAnswers.ElementAt(0);
+
+            radioButton2.Text = SelectAnswers.ElementAt(1);
+
+            radioButton3.Text = SelectAnswers.ElementAt(2);          
+        }
+    }
+
+    // Структура ответов
+    public struct Answers
+    {
+        public int QuestionNumber { get; set; }
+
+        public string AnswerText { get; set; }
+
+        public int AnswerCorrect { get; set; }
+    }
+
+    // СТруктура вопросов
+    public struct Questions
+    {
+        public int QuestionNumber { get; set; }
+
+        public string QuestionText { get; set; }
+    }
+
+}
